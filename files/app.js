8b1086c960cbce2d8cd031dc8ca9564603e9167e
Farah naz <aahmadkhan156@gmail.com>
2022-06-09 22:18:27 +0500

    Complete

--- files/app.js
+++ files/app.js
@@ -27,51 +27,6 @@ function Clear()
     var inputSearch= document.getElementById('input-search');
     inputSearch.value = '';
 }
-
-var main = document.getElementById('mainBox');
-function imgChange(file)
-{
-
-    if(file =='home')
-    {
-
-
-        main.style.background ='linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 1)), url(./images/1.jpg)';
-        styling();
-    }
-    else if(file =='about')
-    {
-        main.style.background ='linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 1)), url(./images/rolex/rolex-milgauss-watches-pictures-p-os-images-311322-org.jpg)';
-        styling();
-
-    }
-    else if(file =='contact')
-    {
-        main.style.background ='linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.75)), url(./images/breguet-watch-4-org.jpg)';
-        styling();
-
-    }
-    else if(file =='products')
-    {
-
-        main.style.background ='linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 1)), url(./images/cartier-metiers-d-art-rotonde-de-cartier-watch-hpi00689-org.jpg)';
-        styling();
-    }
-    else return;
-// 
-
- }
- 
- function styling()
- {
-
-    main.style.transition = '0.5s'
-    // main.style.backgroundAttachment = 'fixed';
-    main.style.backgroundPosition = 'center center';
-    main.style.backgroundSize = 'cover';
-    main.style.backgroundRepeat = 'no-repeat';
-    
- }
  
 
  //****************  MODAL ****************** */
@@ -81,7 +36,7 @@ function imgChange(file)
  var productDetail = document.getElementById('productDetail')
  function Details(e, img){
      
-
+console.log('hello',img)
 
     productImg.src=img;
 
