79b7dea803de10352df4d1a225156938b5d73450
Farah naz <aahmadkhan156@gmail.com>
2022-06-12 09:29:39 +0500

    Complete

--- files/app.js
+++ files/app.js
@@ -31,13 +31,17 @@ function Clear()
 
  //****************  MODAL ****************** */
 
- var productImg = document.getElementById('productImg');
- var productName   = document.getElementById('productName')
- var productDetail = document.getElementById('productDetail')
  function Details(e, img){
      
-console.log('hello',img)
-
+     
+     
+     
+     var productImg = document.getElementById('productImg');
+     var productName   = document.getElementById('productName')
+     var productDetail = document.getElementById('productDetail')
+     
+     console.log('hello',img,e,productImg,productName,productDetail)
+     
     productImg.src=img;
 
         productName.innerHTML= e.parentNode.firstElementChild.innerHTML;
@@ -45,3 +49,52 @@ console.log('hello',img)
     console.log(productName.innerHTML);
     console.log(productDetail.innerHTML)
  }
+
+ // Contact form Validation
+ 
+ var userName = document.getElementById('name')
+ var phNo    = document.getElementById('phNo');
+ var email   = document.getElementById('email');
+ var subject = document.getElementById('subject');
+ var message = document.getElementById('message');
+ 
+function CheckInfo(){
+
+    if(userName.value.length<3 || userName.value.length>22)
+    {
+        alert('Please Fill Correct Name');
+    }
+     if(phNo.value.length != 11 )
+    {
+        alert('Please Enter Your Corrct Number');
+
+    }
+    if(subject.value.length <15)
+    {
+        alert('Please Write Subject');
+    }
+    if(!message.value )
+    {
+        alert("Please Write Your Message")
+    }
+    else { var flag = 0;
+        for(var i =0 ; i <email.value.length; i++)
+        {
+            if(email.value[i] == '@')
+            {
+                flag = 1;
+            }
+        }
+        if(flag==0)
+        {
+            alert('Enter Your Correct Email');
+        }
+    }
+
+    userName.value = "";
+    phNo.value = '';
+    email.value = '';
+    subject.value = '';
+    message.value = ''
+}
+ 
